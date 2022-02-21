3efe77f5e9a79eb3640dbfbf7bd368a9b6bd78d7
Scott Hill <scott.c.hill.jr@gmail.com>
2022-02-20 18:27:39 -0600

    commit tree 16eb473d9919f14f0fa33233e668b8d47f4d01a9

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word.length <= 1) {
+    return true
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      return isPalindrome(word.slice(1, -1))
+    } else {
+      return false
+    }
+  }
 }
 
 /* 
@@ -7,16 +15,25 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  If the length of a word is less than or equal to 1 letter, it is automatically a palindrome like "a",
+  otherwise if a word with no array is equal to the array of a word that is the length
+  of the word from the end (-1) then a palindrome is returned by slicing both ends of the word
+  to be read backward; anything else is automatically false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
+
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("alien"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
